f48220a496a08eb36f69deb409a4afa6da98e371
karolinaconk <karolina14badilla@gmail.com>
2023-05-06 00:16:47 -0700

    agregando matriz

--- the-game-of-life/Form1.Designer.cs
+++ the-game-of-life/Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.InitialImage = global::the_game_of_life.Properties.Resources._284011742_3152553471727958_8913141010978702565_n;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Random";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(337, 418);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Iniciar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(256, 418);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Pausar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(429, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "The game of life";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- the-game-of-life/Form1.cs
+++ the-game-of-life/Form1.cs
@@ -12,9 +12,56 @@ namespace the_game_of_life
 {
     public partial class Form1 : Form
     {
+        private int longitud = 40;
+        private int longitudPixel = 10;
+        int[,] celulas;
         public Form1()
         {
             InitializeComponent();
+
+            celulas = new int[longitud, longitud];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PintarMatriz()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            for (int x = 0; x < longitud; x++)
+            {
+                for (int y = 0; y < longitud; y++)
+                {
+                    if (celulas[x,y] == 0)
+                        PintarPixel(bmp, x, y, Color.White);
+                    else
+                        PintarPixel(bmp, x, y, Color.Black);
+                }
+            }
+        }
+        private void PintarPixel(Bitmap bmp, int x, int y, Color color)
+        {
+            for (int i=0; i<longitudPixel; i++)
+            {
+                for (int j = 0; j < longitudPixel; j++)
+                bmp.SetPixel(i + (x*longitudPixel),j+(y*longitudPixel),color);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // reiniciar
+            for (int i = 0; i < longitud; i++)
+                for (int j = 0; j < longitud; j++)
+                    celulas[i, j] = 0;
+
+            Random r = new Random();
+            // llenar random
+            for (int i = 0; i < longitud; i++)
+                for (int j = 0; j < longitud; j++)
+                    celulas[i, j] = r.Next(0,2);
         }
     }
 }
